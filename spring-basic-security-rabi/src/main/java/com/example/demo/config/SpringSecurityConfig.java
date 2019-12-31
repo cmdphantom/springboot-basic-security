ce3894607f9429968042c5ab6b0bfbbb63e9d4cd
“djrabi007” <rabi007007@gmail.com>
2020-01-01 04:57:51 +0530

    Spring Security for Auth URL and Admin user

---
+++ spring-basic-security-rabi/src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig 
+		extends WebSecurityConfigurerAdapter{
+
+	// 2 ROLE defined for AUTH user!!!
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+					.withUser("Roddur").password("p")
+					.roles("ADMIN");  //ADMIN Role
+		
+		auth.inMemoryAuthentication()
+				.withUser("Swagata").password("p")
+				.roles("USER"); //USER Role ....
+	}
+	
+	
+	
+	
+	// Case#1 - Allowed Any URL  (Auth+No Auth ) .."/auth" and "/noAuth"
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+	}*/
+	
+	//Case#2A - Allowed AUTH  ONLY!!!   "/auth" -****ADMIN+USER Role****
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/auth/**").fullyAuthenticated().and().httpBasic();
+	}*/
+	
+	//Case#2B - Allowed AUTH  ONLY!!!   "/auth" -****ADMIN ONLY****
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.authorizeRequests()
+			.antMatchers("/auth/**")
+			.hasAnyRole("ADMIN").anyRequest()
+			.fullyAuthenticated().and().httpBasic();
+		}
+	
+	
+	
+	@Bean
+	private static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder)NoOpPasswordEncoder.getInstance();
+
+	}
+	
+	
+}
